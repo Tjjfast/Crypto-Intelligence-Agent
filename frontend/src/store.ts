a717c88835321945f3e77fe567305761b796d90f
Tjfast <tj.fast005@gmail.com>
2025-10-02 23:51:52 +0530

    Update frontend

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -79,7 +79,7 @@ export const useStore = create<Store>()(
             typeof messages === 'function' ? messages(state.messages) : messages
         })),
       chatInputRef: { current: null },
-      selectedEndpoint: 'http://localhost:7777',
+      selectedEndpoint: 'https://crypto-intelligence-agent-1.onrender.com',
       setSelectedEndpoint: (selectedEndpoint) =>
         set(() => ({ selectedEndpoint })),
       agents: [],
